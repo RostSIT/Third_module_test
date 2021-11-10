5136b837be0445331d324c6d17efa3d802bdfcf8
RostSIT <streltsovrp@gmail.com>
2021-11-10 19:00:40 +0200

    The end

--- Test-task-Tracki/test_CheckThatAllMostImportantElementsArePresent.py
+++ Test-task-Tracki/test_CheckThatAllMostImportantElementsArePresent.py
@@ -5,72 +5,26 @@ import math
 from selenium.webdriver.common.by import By
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="module")
 def browser():
     print("\nstart browser for test..")
     browser = webdriver.Chrome()
+
     yield browser
     print("\nquit browser..")
     browser.quit()
 
-link = "https://www.google.com/"
 
-@pytest.mark.parametrize('element', ["/html/body/div[1]/div[2]/div/img", "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"])
-                                  # "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[3]/div[2]/span", "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[3]/div[3]/svg/path[1]",
-                                  # "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[3]/div[3]/svg/path[1]", "https://stepik.org/lesson/236903/step/1",
-                                  # "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]", "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[2]"])
-
-
+@pytest.fixture(scope="module")
+def open_test_page(browser):
+    browser.get("https://www.google.com/")
+    browser.implicitly_wait(10)
+    return browser
 
 
-class TestLogin:
-
-
-
-    def test_guest_should_see_login_link(self, browser, element, en):
-        browser.get(link)
-
-        browser.implicitly_wait(10)
-
-        inputAnswer = browser.find_element_by_xpath(element)
-        assert inputAnswer == en, "Should google element"
-
-
-
-# a = TestLogin()
-# a.answer()
-# a.test_guest_should_see_login_link()
-
-
-
-
-
-
-# --------------------------------------------------
-
-
-# element = ''
-#
-#
-# def test_searchElements(browser):
-#     browser.get(link)
-#     browser.find_element_by_css_selector(element)
-#
-#
-# def xxx():
-#     element = '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input'
-#     json_response = requests.get(test_link + '/branches').json()
-#     assert 4 == len(json_response)
-#
-#
-# @pytest.mark.parametrize("expected_branches", ['dependabot/pip/docs/django-3.1.13',
-#                                                'dependabot/pip/docs/pillow-8.3.2',
-#                                                'develop',
-#                                                'master'])
-# def test_check_branches_names(expected_branches):
-#     existing_branches_names = []
-#     json_response = requests.get(test_link + '/branches').json()
-#     for branch in json_response:
-#         existing_branches_names.append((branch['name']))
-#     assert expected_branches in existing_branches_names
-#
+@pytest.mark.parametrize('element', ["/html/body/div[1]/div[2]/div/img", "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input",
+                                     "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[3]/div[2]/span", "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[3]/div[3]/svg/path[1]",
+                                     "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[3]/div[3]/svg/path[1]", "https://stepik.org/lesson/236903/step/1",
+                                     "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]", "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[2]"])
+def test_check_google_main_page(open_test_page, element):
+    assert open_test_page.find_element_by_xpath(element).is_displayed(), "Element was not found"
