46ba7da0b80ae0d5a08b2d314507e1c81490eec4
RostSIT <streltsovrp@gmail.com>
2021-11-10 20:35:54 +0200

    Add test_check_branches_names and branches_schema.json

--- Test-task-Tracki/test_AmountOfOpenPullRequests.py
+++ Test-task-Tracki/test_AmountOfOpenPullRequests.py
@@ -1,5 +1,7 @@
+import json
 import requests
 import pytest
+import jsonschema
 
 
 test_link = "https://api.github.com/repos/Fantomas42/django-blog-zinnia"
@@ -25,3 +27,10 @@ def test_check_branches_names(expected_branches):
     for branch in json_response:
         existing_branches_names.append((branch['name']))
     assert expected_branches in existing_branches_names
+
+
+def test_check_branches_schema():
+    response = requests.get(test_link + '/branches').json()
+    with open('branches_schema.json') as json_file:
+        schema = json.load(json_file)
+    jsonschema.validate(instance=response, schema=schema)
