e4511ff1b1f4a7743c882d672a009215324d30fd
RostSIT <streltsovrp@gmail.com>
2021-11-10 13:25:57 +0200

    1.1 Task upgreat

--- Test-task-Tracki/111.py
+++ Test-task-Tracki/111.py
@@ -19,5 +19,9 @@ def test_guest_should_see_login_link(browser):
     browser.get(link)
     a = browser.find_element_by_css_selector("#pull-requests-repo-tab-count.Counter")
     count = a.text
-    r = requests.get('https://api.github.com/repos/:owner/:repo/pulls?state=all')
-    len(r.json()) == count
+
+    r = requests.get('https://api.github.com/repos/torvalds/linux/pulls').text
+    # len(r.json()) == int(count)
+    print(f'\n{r}')
+    print(f'\n{count}')
+
