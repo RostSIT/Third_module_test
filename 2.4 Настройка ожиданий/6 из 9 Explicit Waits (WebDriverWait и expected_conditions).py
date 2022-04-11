817d84f247475bd0f5552feeef3bfca9cab49cda
RostSIT <Git2020Rost>
2022-04-11 04:52:00 +0300

    Commented out the non-working version of the code

--- 2.4 Настройка ожиданий/6 из 9 Explicit Waits (WebDriverWait и expected_conditions).py
+++ 2.4 Настройка ожиданий/6 из 9 Explicit Waits (WebDriverWait и expected_conditions).py
@@ -22,12 +22,10 @@ no such element: Unable to locate element:
 {"method":"id","selector":"verify_message"}
 '''
 
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
-
 
 browser = webdriver.Chrome()
 
@@ -39,7 +37,6 @@ button.click()
 message = browser.find_element_by_id("verify_message")
 
 assert "successful" in message.text
-
 
 '''
 Как вы видите, в этом случае нужно использовать поиск элементов с помощью класса By, который мы рассмотрели ранее. element_to_be_clickable вернет элемент, когда он станет кликабельным, или вернет False в ином случае.
